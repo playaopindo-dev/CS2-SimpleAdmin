9b6f6cdbd668dfd1c310442a3f4208b0e368eb9b
playaopindo-dev <playaopindo@gmail.com>
2026-06-22 18:58:33 +0200

    Use DTO mapping for SQLite IP history rows

--- CS2-SimpleAdmin/Models/IpHistoryRow.cs
+++ CS2-SimpleAdmin/Models/IpHistoryRow.cs
@@ -2,16 +2,13 @@ namespace CS2_SimpleAdmin.Models;
 
 public sealed class IpHistoryRow
 {
-    public IpHistoryRow(long steamid, string? name, long address, DateTime used_at)
-    {
-        Steamid = Convert.ToUInt64(steamid);
-        Name = name;
-        Address = Convert.ToUInt32(address);
-        Used_at = used_at;
-    }
+    public long steamid { get; set; }
+    public string? name { get; set; }
+    public long address { get; set; }
+    public DateTime used_at { get; set; }
 
-    public ulong Steamid { get; }
-    public string? Name { get; }
-    public uint Address { get; }
-    public DateTime Used_at { get; }
+    public ulong Steamid => Convert.ToUInt64(steamid);
+    public string? Name => name;
+    public uint Address => Convert.ToUInt32(address);
+    public DateTime Used_at => used_at;
 }
